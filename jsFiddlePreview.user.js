81c06cd80474401c4693b3dc69201d8b95d08bf6
Sergey Shchur <sergey.shchur@gmail.com>
2012-06-25 16:59:38 +0300

    added first version of script

---
+++ jsFiddlePreview.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name          Stackoverflow jsFiddle viewer
+// @author        Sergey Shchur aka antyrat (http://stackoverflow.com/users/186535/antyrat)
+// @description   Adds embedded jsFiddle content to the questions/answers posts. 
+// @include       http://www.stackoverflow.com/questions*
+// @include       http://stackoverflow.com/questions/*
+// @version       1.0
+// ==/UserScript==
+
+// insert jQuery to the script
+var script = document.createElement( "script" );
+script.type = "text/javascript";
+script.textContent = "(" + addJsFiddlePreview.toString() + ")( jQuery )";
+document.body.appendChild(script);
+
+function addJsFiddlePreview( $ ) {
+	
+	// Iterate thru questions and answers body
+	$( '.post-text' ).each( function( ) {
+		var postLinks = $( this ).find( 'a[href]' ),
+		  postContainer = this;
+		
+		if( postLinks ) {
+			// collect all href links in answers and (or) question
+			postLinks.each( function( ) {
+				var jsFiddleLink = $( this ).attr( 'href' ).match( /http:\/\/jsfiddle\.net\/([A-Za-z0-9]+)/ );
+				
+				if( jsFiddleLink && jsFiddleLink[ 0 ] && jsFiddleLink[ 1 ] ) {
+					
+					// append button
+					var buttonContainer = $( '<div></div>' ).appendTo( postContainer ),
+					  jsFiddleButton = $( '<button data-visible="false">Show jsFiddle ( ' + jsFiddleLink[ 1 ] + ' ) result</button>' ).appendTo( buttonContainer );
+					
+					// show embedded jsFiddle at bottom
+					jsFiddleButton.click( function(  ) {
+						if( $( this ).data( 'visible' ) === false) {
+							$( this ).addClass( 'disabled-button' ).data( 'visible', 'true' );
+							$( '<div><iframe src="' + jsFiddleLink[0] + '/embedded/" width="650" height="400" style="border:2px solid #3D668F;"></iframe></div>' ).appendTo( postContainer );
+						}
+					});
+				}
+			});
+		}
+	});
+}
